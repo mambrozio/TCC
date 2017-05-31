14fea31b72c561398e0cb66b63289922d1e465dd
Matheus Coelho Ambrozio <mambrozio@users.noreply.github.com>
2017-05-30 21:13:09 -0300

    Initial commit for step function creation

---
+++ step.cpp
@@ -0,0 +1,224 @@
+/*
+* File: onefunction.cpp
+* Author: Matheus Coelho Ambrozio
+* Date: May, 2017
+*
+* To compile , execute on terminal :
+* clang++ -o step step.cpp `llvm-config --cxxflags --ldflags --libs all --system-libs`
+*/
+
+#include <memory>
+
+// printing tests
+#include <iostream>
+//
+
+// LLVM includes
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Module.h>
+#include <llvm/Support/TargetSelect.h>
+#include <llvm/Support/DynamicLibrary.h>
+//
+
+#define OP_RETURN 38
+
+#define SIZE_A          8
+#define SIZE_B          9
+#define SIZE_C          9
+
+#define SIZE_OP         6
+
+#define POS_OP          0
+#define POS_A           (POS_OP + SIZE_OP)
+#define POS_C           (POS_A + SIZE_A)
+#define POS_B           (POS_C + SIZE_C)
+
+
+
+// --- Type definitions copied from c-minilua.c
+typedef uint8_t Byte;
+typedef int32_t Int;
+typedef uint32_t Instruction;
+typedef int64_t lua_integer;
+typedef double lua_float;
+
+typedef enum { SHORT_STRING, LONG_STRING } StringType;
+typedef struct {
+    StringType typ;
+    const char *str;
+} String;
+
+typedef struct {
+    int typ;
+    union {
+        int b;
+        lua_integer i;
+        lua_float n;
+    } u;
+} Value;
+
+// Function prototype
+typedef struct Proto {
+    Byte numparams;  /* number of fixed parameters */
+    Byte is_vararg;
+    Byte maxstacksize;  /* number of registers needed by this function */
+    Int sizeupvalues;  /* size of 'upvalues' */
+    Int sizek;  /* size of 'k' */
+    Int sizecode;
+    //Int sizelineinfo;
+    Int sizep;  /* size of 'p' */
+    //Int sizelocvars;
+    Int linedefined;  /* debug information  */
+    Int lastlinedefined;  /* debug information  */
+    Value *k;  /* constants used by the function */
+    Instruction *code;  /* opcodes */
+    //struct Proto **p;  /* functions defined inside the function */
+    //int *lineinfo;  /* map from opcodes to source lines (debug information) */
+    //LocVar *locvars;  /* information about local variables (debug information) */
+    //Upvaldesc *upvalues;  /* upvalue information */
+    //struct LClosure *cache;  /* last-created closure with this prototype */
+    String  *source;  /* used for debug information */
+    //GCObject *gclist;
+} Proto;
+
+//MiniLuaState Struct
+typedef struct MiniLuaState {
+    Proto *proto;
+    Value *registers;
+    size_t return_begin;
+    size_t return_end;
+} MiniLuaState;
+// --- End of type definitions
+
+
+
+// size_t step(MiniLuaState *mls, Instruction inst, uint32_t op, Value *constants) {
+// }
+
+
+int main() {
+
+    //necessary LLVM initializations
+    llvm::InitializeNativeTarget();
+    llvm::InitializeNativeTargetAsmPrinter();
+    llvm::InitializeNativeTargetAsmParser();
+
+    //create the context and the main module
+    llvm::LLVMContext context;
+    std::unique_ptr<llvm::Module> Owner(new llvm::Module("step_module", context));
+    llvm::Module *module = Owner.get();
+
+
+    // --- Declarations
+    //treating string_type enum as an int32
+
+    //create String struct
+    llvm::StructType *string_struct_type = llvm::StructType::create(context, "String");
+    llvm::PointerType *p_string_struct_type = llvm::PointerType::get(string_struct_type, 0);
+    std::vector<llvm::Type *> string_elements;
+    string_elements.push_back(llvm::Type::getInt32Ty(context));   //typ
+    string_elements.push_back(llvm::Type::getInt8PtrTy(context)); //*str
+    string_struct_type->setBody(string_elements);
+    // std::cout << "Struct String dump:\n";
+    // string_struct_type->dump();
+    // std::cout << "\n";
+
+    //create Value struct
+    llvm::StructType *value_struct_type = llvm::StructType::create(context, "Value");
+    llvm::PointerType *p_value_struct_type = llvm::PointerType::get(value_struct_type, 0);
+    std::vector<llvm::Type *> value_elements;
+    value_elements.push_back(llvm::Type::getInt32Ty(context)); //typ
+    value_elements.push_back(llvm::Type::getInt64Ty(context)); //u
+    value_struct_type->setBody(value_elements);
+    // std::cout << "Struct Value dump:\n";
+    // value_struct_type->dump();
+    // std::cout << "\n";
+
+    //create Proto struct
+    llvm::StructType *proto_struct_type = llvm::StructType::create(context, "Proto");
+    llvm::PointerType *p_proto_struct_type = llvm::PointerType::get(proto_struct_type, 0);
+    std::vector<llvm::Type *> proto_elements;
+    proto_elements.push_back(llvm::Type::getInt8Ty(context));     //numparams
+    proto_elements.push_back(llvm::Type::getInt8Ty(context));     //is_vararg
+    proto_elements.push_back(llvm::Type::getInt8Ty(context));     //maxstacksize
+    proto_elements.push_back(llvm::Type::getInt32Ty(context));    //sizeupvalues
+    proto_elements.push_back(llvm::Type::getInt32Ty(context));    //sizek
+    proto_elements.push_back(llvm::Type::getInt32Ty(context));    //sizecode
+    proto_elements.push_back(llvm::Type::getInt32Ty(context));    //sizep
+    proto_elements.push_back(llvm::Type::getInt32Ty(context));    //linedefined
+    proto_elements.push_back(llvm::Type::getInt32Ty(context));    //lastlinedefined
+    proto_elements.push_back(p_value_struct_type);                //*k
+    proto_elements.push_back(llvm::Type::getInt32PtrTy(context)); //*code
+    proto_elements.push_back(p_string_struct_type);               //*source
+    proto_struct_type->setBody(proto_elements);
+    // std::cout << "Struct Proto dump:\n";
+    // proto_struct_type->dump();
+    // std::cout << "\n";
+
+    //create MiniLuaState struct
+    llvm::StructType *miniluastate_struct_type = llvm::StructType::create(context, "MiniLuaState");
+    llvm::PointerType *p_miniluastate_struct_type = llvm::PointerType::get(miniluastate_struct_type, 0);
+    std::vector<llvm::Type *> miniluastate_elements;
+    miniluastate_elements.push_back(p_proto_struct_type);             //*proto
+    miniluastate_elements.push_back(p_value_struct_type);             //*value
+    miniluastate_elements.push_back(llvm::Type::getInt64Ty(context)); //return_begin
+    miniluastate_elements.push_back(llvm::Type::getInt64Ty(context)); //return_end
+    miniluastate_struct_type->setBody(miniluastate_elements);
+    // std::cout << "Struct MiniLuaState dump:\n";
+    // miniluastate_struct_type->dump();
+    // std::cout << "\n";
+
+    //create declaration for step
+    //int step(MiniLuaState *mls, Instruction inst, uint32_t op, Value *constants);
+    std::vector<llvm::Type *> args;
+    args.push_back(p_miniluastate_struct_type);      //*mls
+    args.push_back(llvm::Type::getInt32Ty(context)); //inst
+    args.push_back(llvm::Type::getInt32Ty(context)); //op
+    args.push_back(p_value_struct_type);             //constants
+    llvm::FunctionType *step_type = llvm::FunctionType::get(llvm::Type::getInt64Ty(context), args, false);
+    auto step_func = llvm::Function::Create(step_type, llvm::Function::ExternalLinkage, "step", module);
+
+    //get arguments
+    auto argiter = step_func->arg_begin();
+    llvm::Value *_mls = &*argiter++;
+    llvm::Value *_inst = &*argiter++;
+    llvm::Value *_op = &*argiter++;
+    llvm::Value *_constants = &*argiter++;
+    //_mls->setName("arg");
+
+    std::cout << "Arguments received by interpret function dump:\n";
+    _mls->dump();
+    //std::cout << _mls;
+    std::cout << "\n";
+
+
+    //create irbuilder
+    llvm::IRBuilder<> builder(context);
+
+
+    // --- Creating Blocks
+    //create the entry block
+    auto entry_block = llvm::BasicBlock::Create(context, "entry", step_func);
+
+
+
+    // --- Create code for the entry block
+    builder.SetInsertPoint(entry_block);
+
+
+
+
+
+    builder.CreateRetVoid();
+
+
+
+    //dump module to check ir
+    std::cout << "Module:\n";
+    module->dump();
+    std::cout << "\n";
+
+    return 0;
+}
+
